a4dbec5db25236c73f30068b2f0721639ee8911e
yet <jacob.t.wright@protonmail.com>
2022-03-14 00:43:14 -0500

    sub pages for images made

--- pages/index.js
+++ pages/index.js
@@ -1,141 +1,135 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import path from 'path';
-import fs from 'fs';
+import path from 'path'
+import fs from 'fs'
+import Link from 'next/link'
 
 export default function Home({ images }) {
-
   const getRandomMask = () => {
-    return ("photo-mask--" + Math.floor(Math.random() * 5));
+    return 'photo-mask'
   }
 
-
   const getRandomPos = () => {
-    return ("photo-pos--" + Math.floor(Math.random() * 5));
+    return 'photo-pos--' + Math.floor(Math.random() * 5)
+  }
+
+  const getRandomLeftMargin = () => {
+    return { marginLeft: Math.floor(Math.random() * 500) }
+  }
+
+  const getRandomTopMargin = () => {
+    return { marginTop: Math.floor(Math.random() * 500) }
   }
 
   return (
-    <div className="container">
+    <div className="container custom-container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title index-title">
-          Home to the work of Sophie Cole
-        </h1>
+        <h1 className="title index-title">Home to the work of Sophie Cole</h1>
         <div className="imageWrap">
           {images.map((image, index) => {
             return (
-              <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}>
-                <Image alt="photo of vietnam city" src={'/images/' + image} key={index} width={328} height={226} />
+              <div
+                className={
+                  'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()
+                }
+                style={{
+                  marginTop: Math.floor(Math.random() * 400) + 'px',
+                  marginLeft: Math.floor(Math.random() * 400) + 'px'
+                }}
+              >
+                <Link href={`/posts/${image.split('.')[0]}-page`}>
+                  <a>
+                    <Image
+                      src={'/images/' + image}
+                      key={index}
+                      width={328}
+                      height={226}
+                      objectFit="cover"
+                    />
+                  </a>
+                </Link>
               </div>
             )
           })}
-
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
-
-          {/* <div className={'photo-masked--wrap ' + getRandomMask() + ' ' + getRandomPos()}> */}
-          {/*   <Image alt="photo of vietnam city" src="/images/sohpie-0.jpg" width={328} height={226} /> */}
-          {/* </div> */}
         </div>
       </main>
 
-      <style jsx>{`
+      <style jsx>
+        {`
+          main {
+            height: 100vh;
+            overflow: auto;
+          }
 
-        main {
-          height: 100vh;
-          width: 100vw;
-          overflow:auto;
-        }
+          .index-title {
+            position: sticky;
+            top: calc(100vh - 65px);
+            padding: 8px;
+            color: rgba(21, 23, 25, 0.79);
+            width: 330px;
+            left: 0;
+            z-index: 2;
+          }
 
-        .index-title {
-          position: sticky;
-          top: calc(100vh - 65px);
-          background: white;
-          padding:8px;
-          width: 330px;
-          left:0;
-          z-index:2;
-        }
+          .imageWrap {
+            display: grid;
+            width: 220vw;
+            grid-template-columns: repeat(4, auto);
+            grid-template-rows: repeat(8, auto);
+            margin-top: 21px;
+            grid-gap: 12px;
+          }
 
-        .imageWrap {
-          display: grid;
-          width: 120vw;
-          grid-template-columns: repeat(4, auto);
-          grid-template-rows: repeat(3, auto);
-          margin-top: 21px;
-          grid-gap: 12px;
-        }
+          @media only screen and (max-width: 1000px) {
+            .imageWrap {
+              grid-template-columns: 1fr 1fr;
+              grid-template-rows: auto;
+            }
 
-        .photo-masked--wrap {
-          justify-self: center;
-        }
+            .photo-masked--wrap {
+              margin: 0px !important;
+              justify-self: center;
+            }
+          }
 
-        .photo-pos--0 {
-          grid-row: 2;
-          margin-left: 121px;
-          margin-top: 12px;
-          align-self: end;
-          jusify-self: start;
-        
-        }
+          @media only screen and (max-width: 500px) {
+            .imageWrap {
+              grid-template-columns: 1fr;
+            }
+          }
 
-        .photo-pos--1 {
-          grid-row: 3;
-          margin-left: 162px;
-          grid-column-start: span 2;
-          margin-top: -12px
-        }
+          .photo-masked--wrap {
+            justify-self: center;
+          }
+          .photo-masked--wrap {
+          }
+          .photo-pos--0 {
+            align-self: end;
+            jusify-self: start;
+          }
 
-        .photo-pos--2 {
-          margin-left: 200px;
-          align-self: center;
-          grid-row-start: span 1; 
-          jusify-self: center;
-        }
+          .photo-pos--1 {
+            align-self: start;
+            jusify-self: end;
+          }
 
-        .photo-pos--3 {
-          align-self: end;
-          grid-column-start: span 2;
-          grid-row-start:span 1;
-        }
+          .photo-pos--2 {
+            align-self: center;
+          }
 
-        .photo-pos--4 {
-          align-self: start;
-          margin-left: 32px;
-          margin-top: 40px;
-        }
-    `}
+          .photo-pos--3 {
+            align-self: end;
+          }
+
+          .photo-pos--4 {
+            align-self: start;
+          }
+        `}
       </style>
     </div>
   )
---
+++ pages/posts/[image].js
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Post = () => {
+  const router = useRouter()
+  const { image } = router.query
+
+  return (
+    <div>
+      <h1>{image.split('-')[0]}</h1>
+      <Image
+        width="500"
+        height="500"
+        objectFit="contain"
+        src={'/images/' + image.split('-')[0] + '.jpg'}
+      />
+      <Link href="/">
+        <a>continue exploring</a>
+      </Link>
+
+      <style jsx>{`
+        div {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        p {
+          font-size: 20px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Post
